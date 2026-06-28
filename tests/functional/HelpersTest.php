16c81acfd5350e74ae7e18771b5abb99d96314fe
Alyson Gisely Perez Flores <118328215+alysongiselypf@users.noreply.github.com>
2026-06-28 13:55:48 -0500

    Update HelpersTest.php

--- tests/functional/HelpersTest.php
+++ tests/functional/HelpersTest.php
@@ -1,8 +1,5 @@
 <?php
-
 use PHPUnit\Framework\TestCase;
-
-use NombreDeNamespace\Helpers;
 
 class HelpersTest extends TestCase
 {
@@ -10,52 +7,42 @@ class HelpersTest extends TestCase
     {
         $this->assertTrue(validarTelefono('987654321'));
     }
-
     public function testValidarTelefonoConLetras()
     {
         $this->assertFalse(validarTelefono('98765abcd'));
     }
-
     public function testValidarTelefonoCortoEsInvalido()
     {
         $this->assertFalse(validarTelefono('12345'));
     }
-
     public function testValidarRolUsuarioValidoPaciente()
     {
         $this->assertTrue(validarRolUsuario('paciente'));
     }
-
     public function testValidarRolUsuarioValidoDoctor()
     {
         $this->assertTrue(validarRolUsuario('doctor'));
     }
-
     public function testValidarRolUsuarioInvalido()
     {
         $this->assertFalse(validarRolUsuario('superadmin'));
     }
-
     public function testValidarTipoDocumentoValido()
     {
         $this->assertTrue(validarTipoDocumento('DNI'));
     }
-
     public function testValidarTipoDocumentoInvalido()
     {
         $this->assertFalse(validarTipoDocumento('RUC'));
     }
-
     public function testValidarPasswordCorrecta()
     {
         $this->assertTrue(validarPassword('123456'));
     }
-
     public function testValidarPasswordCorta()
     {
         $this->assertFalse(validarPassword('123'));
     }
-
     public function testCalcularSubtotalCarrito()
     {
         $items = [
@@ -64,17 +51,18 @@ class HelpersTest extends TestCase
         ];
         $this->assertEquals(36.50, calcularSubtotalCarrito($items));
     }
-
     public function testCalcularSubtotalCarritoVacio()
     {
         $this->assertEquals(0.0, calcularSubtotalCarrito([]));
     }
-
     public function testFormatearNombreCompleto()
     {
         $this->assertEquals('Alyson Perez Flores', formatearNombreCompleto('Alyson', 'Perez Flores'));
     }
 }
+
+
+
 
 
 
